be4213413c32220ac49f91943889c7c99c39f973
Eden <gresseden@gmail.com>
2019-10-07 07:56:57 -0400

    10/07/19

--- public/js/formsResponse.js
+++ public/js/formsResponse.js
@@ -1,11 +1,31 @@
 console.log("Hello")
 
-//var db = firebase.firestore();
 
-db.collection("clients")
-  .get()
-  .then(querySnapshot => {
+let tableForm = document.querySelector("#table");
+
+db.collection("clients").onSnapshot(
+  (querySnapshot => {
+    tableForm.innerHTML = '';
     querySnapshot.forEach(doc => {
       console.log(`${doc.id} => ${doc.data()}`);
-    });
-  });
+      tableForm.innerHTML += `
+        <tr>
+            <th class="row">${doc.id}</th>
+                <td>${doc.data().companyName}</td>
+                <td>${doc.data().contacName}</td>
+                <td>${doc.data().location}</td>
+                <td>${doc.data().phoneContact}</td>
+                <td>${doc.data().city}</td>
+                <td>${doc.data().state}</td>
+                <td>${doc.data().zip}</td>
+        </tr>
+      
+      `;
+
+
+                   
+
+});
+}));
+
+  
